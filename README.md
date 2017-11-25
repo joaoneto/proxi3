2cf531cd4f55f5954fa68e83abb407e041c763e1
João Neto <joaopintoneto@gmail.com>
2017-11-25 01:00:45 -0200

    fixed Allow-Control-Allow-Origin same origin policy added config path option

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 proxi3
 ======
 A command-line tool to start a proxy for your server, redirecting all traffic from localhost:3000/\*\*/\* to your-server.com/\*\*/\* for development purposes.
+Before request the server, request headers can be overrided by *proxi3.config.json*, after the server is requested, the response headers: **Origin** and **Access-Control-Allow-Origin** are restaured to original values, this trick prevent browser to fire the *Allow-Control-Allow-Origin same origin policy error*.
 
 ## Install
 ```shell
@@ -9,6 +10,8 @@ npm install -g proxi3
 
 ## Config
 A *proxi3.config.json* can be created in root project and proxi3 will read de config and starts with this configuration.
+
+- **headers**: Overrides the original request headers, to pass to server request.
 
 ### proxi3.config.json example:
 ```json
@@ -26,10 +29,16 @@ A *proxi3.config.json* can be created in root project and proxi3 will read de co
 proxi3 [options] <proxy_api>
 ```
 
+#### Usage example:
+```shell
+proxi3 http://example.com/ --config ~/proxi3.config.json
+```
+
 ### Options:
 ```
-  -V, --version      output the version number
-  -p, --port [port]  Specify proxi3 port (default: 3000)
-  -H, --host [host]  Specify proxi3 host (default: 0.0.0.0)
-  -h, --help         output usage information
+  -V, --version          output the version number
+  -p, --port [port]      specify proxi3 port (default: 3000)
+  -H, --host [host]      specify proxi3 host (default: 0.0.0.0)
+  -c, --config <config>  specify proxi3.config.json path
+  -h, --help             output usage information
 ```
--- index.js
+++ index.js
@@ -8,12 +8,6 @@ const { spawn } = require('child_process');
 const program = require('commander');
 const package = require('./package');
 
-let proxi3Config = {};
-try {
-  proxi3Config = require(path.resolve(cwd, 'proxi3.config'));
-} catch (e) {
-}
-
 let $request = request.defaults({});
 if (process.env.http_proxy) {
   $request = request.defaults({ proxy: process.env.http_proxy });
@@ -27,14 +21,27 @@ program
   .action((argProxyApi) => {
     proxyApi = argProxyApi.replace(/\/$/, '');
   })
-  .option('-p, --port [port]', 'Specify proxi3 port', 3000)
-  .option('-H, --host [host]', 'Specify proxi3 host', '0.0.0.0')
+  .option('-p, --port [port]', 'specify proxi3 port', 3000)
+  .option('-H, --host [host]', 'specify proxi3 host', '0.0.0.0')
+  .option('-c, --config <config>', 'specify proxi3.config.json path')
   .parse(process.argv);
 
 if (typeof proxyApi === 'undefined') {
   console.log('  No <proxy_api> given');
   program.help();
   process.exit(1);
+}
+
+
+const proxi3ConfigPath = program.config ? program.config : path.resolve(cwd, 'proxi3.config');
+let proxi3Config = {};
+try {
+  proxi3Config = require(proxi3ConfigPath);
+} catch (e) {
+  if (program.config) {
+    console.error(`  proxi3 config file not found at: ${proxi3ConfigPath}`);
+    process.exit(1);
+  }
 }
 
 app.use((req, res, next) => {
@@ -45,23 +52,32 @@ app.use((req, res, next) => {
 });
 
 app.use('*', (req, res, next) => {
+  const originalOrigin = req.headers.origin;
   const headers = Object.assign(req.headers, proxi3Config.headers);
+
   Object.keys(headers).forEach((header) => {
     if (!headers[header]) delete headers[header];
   });
 
-  console.log(headers)
   console.log(`[${(new Date()).toISOString()}] [${req.method}] ${proxyApi}${req.originalUrl}`);
 
   req.pipe($request({
     url: `${proxyApi}${req.originalUrl}`,
     headers
-  })).pipe(res);
+  }))
+  .on('response', (response) => {
+    const headers = response.headers;
+    headers['Origin'] = originalOrigin;
+    headers['Access-Control-Allow-Origin'] = originalOrigin;
+    res.writeHead(response.statusCode, headers);
+  }).pipe(res);
 });
 
 app.listen(program.port, program.host, () => {
   console.log(`runing proxi3 on http://${program.host}:${program.port}`);
   console.log(`proxing: ${proxyApi}`);
+  console.log(`trying to use: ${proxi3ConfigPath}`);
+  console.log('');
   console.log("                     _ _____ ");
   console.log(" _ __  _ __ _____  _(_)___ / ");
   console.log("| '_ \\| '__/ _ \\ \\/ / | |_ \\ ");
